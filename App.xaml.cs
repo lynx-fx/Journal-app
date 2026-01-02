30e031aa1677d10c7ad44ab7b3bd15a8f48184c8
lynx-fx <anupbhujel07@gmail.com>
2026-01-02 15:19:56 +0545

    Inital push

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace journal_app;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+	}
+
+	protected override Window CreateWindow(IActivationState? activationState)
+	{
+		return new Window(new MainPage()) { Title = "journal-app" };
+	}
+}
